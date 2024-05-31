15e341bb51aecdcc263b8977b9955a6eacae26e4
VijayBharvad <83714998+vijay0772@users.noreply.github.com>
2024-05-31 14:19:51 -0500

    commit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,6 @@ import Typical from "react-typical";
 import { contactLinks } from "../constants";
 import { ThemeContext } from "../themeProvider";
 import { motion } from "framer-motion";
-import { Link } from "react-scroll";
 import cloud from "../assets/cloudBg.png";
 import cloudDark from "../assets/cloudDark.png";
 import resume from "../assets/resume.pdf"; // Import the resume file
